67a6d3cdc3ccda4b386d65a4cac480ab03a2aa19
Leon <leon.janicki1@wp.pl>
2026-02-06 10:54:23 +0100

    prepare for access token request

--- src/api/httpserver.cpp
+++ src/api/httpserver.cpp
@@ -1,8 +1,10 @@
 #include "yhirose/httplib.h"
+#include "encoder.hpp"
 
 #include <string>
 #include <iostream>
 #include <thread>
+#include <map>
 
 void serverHTMLSetup(std::string state){
   using namespace httplib;
@@ -18,11 +20,25 @@ void serverHTMLSetup(std::string state){
         if(req.has_param("state")){
           req_state = req.get_param_value("state");
         };
-        if (code == ""){
-          res.set_content("Something went wrong","text/plain");
-        } else if(req_state != state) {
-          res.set_content("State mismatch","text/plain");
+        if(req.has_param("error")){
+          auto req_error = req.get_param_value("error");
+          res.set_content(req_error,"text/plain");
+          break;
         }
+
+        if      (req_code == "")     { res.set_content("Something went wrong","text/plain");}
+        else if (req_state != state) { res.set_content("State mismatch","text/plain");}
+
+        else {
+          // TODO: request access token and figure out how this server can run in the background independenly
+          // Client cli;
+
+          // encode_hashmap_ordered("");
+          // cli.send()
+
+        };
+
+
     });
   std::cout << " \n [HTTPserver] Starting on http://127.0.0.1:54789 \n";
   svr.listen("127.0.0.1", 54789);
